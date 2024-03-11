25c7b1b75404642825556055d8d57433cf8d8406
Sagun Rajput <rajput.sagun@indiamart.com>
2024-03-11 18:06:54 +0530

    context api creation

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -17,7 +17,6 @@ const fetchData = async () => {
      const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.65200&lng=77.16630&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
      
      const jsonData = await data.json();
-     console.log(jsonData?.data)
      setrestDataObj(jsonData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
      setfilteredResData(jsonData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
     }
